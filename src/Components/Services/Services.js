f6a60a5e1d69b681d099cceff24c5d170b43441f
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-10-31 15:27:22 +0600

    Service Component Modify

--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import service_image1 from '../../images/pilates.jpg'
 import SingleCard from '../../SingleCard/SingleCard';
-const Services = () => {
+
+const Services = (props) => {
+
+    const [service, setService] = useState([]);
+
+    useEffect(() => {
+        fetch('')
+    }, []);
+
     return (
-        <div className="container py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6" >
-            <SingleCard></SingleCard>
-        </div >
+        <div>
+            <h2>Our Services</h2>
+            <div className="container py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6" >
+                <SingleCard></SingleCard>
+            </div >
+        </div>
+
+
     );
 };
 
