ac57299096ddffe06e8f20314b9dc5788010518f
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-10-31 10:59:52 +0600

    New Components added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header/Header';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Banner from './Components/Banner/Banner';
+import LogoSection from './Components/LogoSection/LogoSection';
+import Services from './Components/Services/Services';
+import Features from './Components/Features/Features';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <h1 className="text-3xl font-bold">
-        Hello world!
-      </h1>
+      <Banner></Banner>
+      <Services></Services>
+      <Features></Features>
+      <LogoSection></LogoSection>
+      <Login></Login>
+      <Footer></Footer>
     </div>
   );
 }
