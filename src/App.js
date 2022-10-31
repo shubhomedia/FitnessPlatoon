6b7f6211051ac03caf56249d324862ad41ef2a72
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-10-31 08:36:45 +0600

    New Plugin Flowbite Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header/Header';
 
 function App() {
   return (
     <div className="App">
-
+      <Header></Header>
+      <h1 className="text-3xl font-bold">
+        Hello world!
+      </h1>
     </div>
   );
 }
