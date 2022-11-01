5cb3d1ca65a757c2f0ab175b81f138efb05ad395
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-01 07:54:43 +0600

    New Component Home Added

--- src/App.js
+++ src/App.js
@@ -7,12 +7,20 @@ import Services from './Components/Services/Services';
 import Features from './Components/Features/Features';
 import Login from './Components/Login/Login';
 import NotFound from './Components/NotFound/NotFound';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './Components/Pages/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Banner></Banner>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
       <Services></Services>
       <Features></Features>
       <LogoSection></LogoSection>
