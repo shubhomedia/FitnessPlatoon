e0096b5ce61c71559471f7cce4bdd418acfcb8af
MD.Owes Quruny Shubho <shubhomedia@gmail.com>
2022-11-01 13:43:35 +0600

    Login Page Modified

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Home from './Components/Pages/Home/Home';
 import About from './Components/Pages/About/About';
 import Services from './Components/Services/Services';
 import Contact from './Components/Pages/Contact/Contact';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
@@ -22,6 +23,9 @@ function App() {
           </Route>
           <Route path="/about">
             <About></About>
+          </Route>
+          <Route path="/login">
+            <Login></Login>
           </Route>
           <Route path="/services">
             <Services></Services>
